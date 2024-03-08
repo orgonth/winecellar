3c6c7315dda079be639da4362efd9ca97294400e
Tommy Carozzani <27284007+orgonth@users.noreply.github.com>
2024-03-08 19:02:00 +0100

    [format] use prettier

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { ApplicationProvider, Button, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import {
+  ApplicationProvider,
+  Button,
+  IconRegistry,
+  Layout,
+} from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { SafeAreaView } from 'react-native';
 import * as eva from '@eva-design/eva';
-import { default as theme } from './style/theme.json'
+import { default as theme } from './style/theme.json';
+import React from 'react';
 
 const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <SafeAreaView>
       <Button>Home</Button>
     </SafeAreaView>
@@ -16,7 +22,7 @@ export default () => {
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme}}>
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
         <HomeScreen />
       </ApplicationProvider>
     </>
